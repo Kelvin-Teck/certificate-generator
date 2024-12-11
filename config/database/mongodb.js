d7a54036b813adc20dd8688236130d6cfc7867cb
Eneh Kelvin Chukwuemeka <kelvineneh4@gmail.com>
2024-12-11 09:48:09 +0100

    finishing Touch

--- config/database/mongodb.js
+++ config/database/mongodb.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDB = async () => {
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect(process.env.MONGODB_URI_DEVELOPMENT, {
+    await mongoose.connect(process.env.MONGODB_URI_PRODUCTION, {
       dbName: "certificate-generator",
     });
     console.log("Database connection established successfully!!!");
